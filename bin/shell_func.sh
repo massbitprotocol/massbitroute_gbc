6be65629e03483474450b18163ddde1e43c5901d
Vu Tran <baysao@gmail.com>
2022-03-23 11:00:40 +0700

    remove double quote in build env.lua

--- bin/shell_func.sh
+++ bin/shell_func.sh
@@ -53,7 +53,7 @@ function loadEnv() {
 		cat $_file | grep -v "^#" | awk -F'=' 'BEGIN{cfg="return {\n"}
 {
 	sub(/^export\s*/,"",$1);
-val_1=subtr($2,0,1);
+val_1=substr($2,0,1);
 print val_1;
 
         cfg=cfg"[\""$1"\"]""=\""$2"\",\n";
